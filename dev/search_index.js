dc6d0c5a7727ce08530237d9b8408763104a67b7
Markus Petters <mdpetter@ncsu.edu>
2020-08-28 19:21:49 -0400

    initial commit

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DifferentialMobilityAnalyzers.jl","page":"Home","title":"DifferentialMobilityAnalyzers.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A Julia package for working with data from aerosol Differential Mobility Analyzers.","category":"page"},{"location":"#Package-Features","page":"Home","title":"Package Features","text":"","category":"section"}]
+}
